cf92fa7a1eed9d3797f9563b29de08ca363aa407
Elias Wernicke <e.w.1111111119@gmail.com>
2025-02-15 18:11:42 +0100

    copy emulated hue component

--- custom_components/integration_blueprint/__init__.py
+++ custom_components/integration_blueprint/__init__.py
@@ -1,79 +1,159 @@
-"""
-Custom integration to integrate integration_blueprint with Home Assistant.
-
-For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
-"""
+"""Support for local control of entities by emulating a Philips Hue bridge."""
 
 from __future__ import annotations
 
-from datetime import timedelta
-from typing import TYPE_CHECKING
+import logging
 
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.loader import async_get_loaded_integration
+from aiohttp import web
+import voluptuous as vol
 
-from .api import IntegrationBlueprintApiClient
-from .const import DOMAIN, LOGGER
-from .coordinator import BlueprintDataUpdateCoordinator
-from .data import IntegrationBlueprintData
+from homeassistant.components.http import KEY_HASS
+from homeassistant.components.network import async_get_source_ip
+from homeassistant.const import (
+    CONF_ENTITIES,
+    CONF_TYPE,
+    EVENT_HOMEASSISTANT_STARTED,
+    EVENT_HOMEASSISTANT_STOP,
+)
+from homeassistant.core import Event, HomeAssistant
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
-if TYPE_CHECKING:
-    from homeassistant.core import HomeAssistant
+from .config import (
+    CONF_ADVERTISE_IP,
+    CONF_ADVERTISE_PORT,
+    CONF_ENTITY_HIDDEN,
+    CONF_ENTITY_NAME,
+    CONF_EXPOSE_BY_DEFAULT,
+    CONF_EXPOSED_DOMAINS,
+    CONF_HOST_IP,
+    CONF_LIGHTS_ALL_DIMMABLE,
+    CONF_LISTEN_PORT,
+    CONF_OFF_MAPS_TO_ON_DOMAINS,
+    CONF_UPNP_BIND_MULTICAST,
+    DEFAULT_LIGHTS_ALL_DIMMABLE,
+    DEFAULT_LISTEN_PORT,
+    DEFAULT_TYPE,
+    TYPE_ALEXA,
+    TYPE_GOOGLE,
+    Config,
+)
+from .const import DOMAIN
+from .hue_api import (
+    HueAllGroupsStateView,
+    HueAllLightsStateView,
+    HueConfigView,
+    HueFullStateView,
+    HueGroupView,
+    HueOneLightChangeView,
+    HueOneLightStateView,
+    HueUnauthorizedUser,
+    HueUsernameView,
+)
+from .upnp import DescriptionXmlView, async_create_upnp_datagram_endpoint
 
-    from .data import IntegrationBlueprintConfigEntry
-
-PLATFORMS: list[Platform] = [
-    Platform.SENSOR,
-    Platform.BINARY_SENSOR,
-    Platform.SWITCH,
-]
+_LOGGER = logging.getLogger(__name__)
 
 
-# https://developers.home-assistant.io/docs/config_entries_index/#setting-up-an-entry
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: IntegrationBlueprintConfigEntry,
-) -> bool:
-    """Set up this integration using UI."""
-    coordinator = BlueprintDataUpdateCoordinator(
-        hass=hass,
-        logger=LOGGER,
-        name=DOMAIN,
-        update_interval=timedelta(hours=1),
+CONFIG_ENTITY_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_ENTITY_NAME): cv.string,
+        vol.Optional(CONF_ENTITY_HIDDEN): cv.boolean,
+    }
+)
+
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Optional(CONF_HOST_IP): cv.string,
+                vol.Optional(CONF_LISTEN_PORT, default=DEFAULT_LISTEN_PORT): cv.port,
+                vol.Optional(CONF_ADVERTISE_IP): cv.string,
+                vol.Optional(CONF_ADVERTISE_PORT): cv.port,
+                vol.Optional(CONF_UPNP_BIND_MULTICAST): cv.boolean,
+                vol.Optional(CONF_OFF_MAPS_TO_ON_DOMAINS): cv.ensure_list,
+                vol.Optional(CONF_EXPOSE_BY_DEFAULT): cv.boolean,
+                vol.Optional(CONF_EXPOSED_DOMAINS): cv.ensure_list,
+                vol.Optional(CONF_TYPE, default=DEFAULT_TYPE): vol.Any(
+                    TYPE_ALEXA, TYPE_GOOGLE
+                ),
+                vol.Optional(CONF_ENTITIES): vol.Schema(
+                    {cv.entity_id: CONFIG_ENTITY_SCHEMA}
+                ),
+                vol.Optional(
+                    CONF_LIGHTS_ALL_DIMMABLE, default=DEFAULT_LIGHTS_ALL_DIMMABLE
+                ): cv.boolean,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def start_emulated_hue_bridge(
+    hass: HomeAssistant, config: Config, app: web.Application
+) -> None:
+    """Start the emulated hue bridge."""
+    protocol = await async_create_upnp_datagram_endpoint(
+        config.host_ip_addr,
+        config.upnp_bind_multicast,
+        config.advertise_ip,
+        config.advertise_port or config.listen_port,
     )
-    entry.runtime_data = IntegrationBlueprintData(
-        client=IntegrationBlueprintApiClient(
-            username=entry.data[CONF_USERNAME],
-            password=entry.data[CONF_PASSWORD],
-            session=async_get_clientsession(hass),
-        ),
-        integration=async_get_loaded_integration(hass, entry.domain),
-        coordinator=coordinator,
-    )
 
-    # https://developers.home-assistant.io/docs/integration_fetching_data#coordinated-single-api-poll-for-data-for-all-entities
-    await coordinator.async_config_entry_first_refresh()
+    runner = web.AppRunner(app)
+    await runner.setup()
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    site = web.TCPSite(runner, config.host_ip_addr, config.listen_port)
+
+    try:
+        await site.start()
+    except OSError as error:
+        _LOGGER.error(
+            "Failed to create HTTP server at port %d: %s", config.listen_port, error
+        )
+        protocol.close()
+        return
+
+    async def stop_emulated_hue_bridge(event: Event) -> None:
+        """Stop the emulated hue bridge."""
+        protocol.close()
+        await site.stop()
+        await runner.cleanup()
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_emulated_hue_bridge)
+
+
+async def async_setup(hass: HomeAssistant, yaml_config: ConfigType) -> bool:
+    """Activate the emulated_hue component."""
+    local_ip = await async_get_source_ip(hass)
+    config = Config(hass, yaml_config.get(DOMAIN, {}), local_ip)
+    await config.async_setup()
+
+    app = web.Application()
+    app[KEY_HASS] = hass
+
+    # We misunderstood the startup signal. You're not allowed to change
+    # anything during startup. Temp workaround.
+    app._on_startup.freeze()  # noqa: SLF001
+    await app.startup()
+
+    DescriptionXmlView(config).register(hass, app, app.router)
+    HueUsernameView().register(hass, app, app.router)
+    HueConfigView(config).register(hass, app, app.router)
+    HueUnauthorizedUser().register(hass, app, app.router)
+    HueAllLightsStateView(config).register(hass, app, app.router)
+    HueOneLightStateView(config).register(hass, app, app.router)
+    HueOneLightChangeView(config).register(hass, app, app.router)
+    HueAllGroupsStateView(config).register(hass, app, app.router)
+    HueGroupView(config).register(hass, app, app.router)
+    HueFullStateView(config).register(hass, app, app.router)
+
+    async def _start(event: Event) -> None:
+        """Start the bridge."""
+        await start_emulated_hue_bridge(hass, config, app)
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, _start)
 
     return True
-
-
-async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: IntegrationBlueprintConfigEntry,
-) -> bool:
-    """Handle removal of an entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-
-async def async_reload_entry(
-    hass: HomeAssistant,
-    entry: IntegrationBlueprintConfigEntry,
-) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,8 +1,6 @@
-"""Constants for integration_blueprint."""
+"""Constants for emulated_hue."""
 
-from logging import Logger, getLogger
+HUE_SERIAL_NUMBER = "001788FFFE23BFC2"
+HUE_UUID = "2f402f80-da50-11e1-9b23-001788255acc"
 
-LOGGER: Logger = getLogger(__package__)
-
-DOMAIN = "integration_blueprint"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+DOMAIN = "emulated_hue"
